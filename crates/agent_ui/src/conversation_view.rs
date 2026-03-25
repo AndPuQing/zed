7afa6eaa58c5392f27c179cda76b59f1f7e9bf79
PuQing <me@puqing.work>
2026-03-25 17:07:05 +0800

    agent_ui: Remove unused SpinnerLabel import

--- crates/agent_ui/src/conversation_view.rs
+++ crates/agent_ui/src/conversation_view.rs
@@ -51,7 +51,7 @@ use theme::AgentFontSize;
 use ui::{
     Callout, CircularProgress, CommonAnimationExt, ContextMenu, ContextMenuEntry, CopyButton,
     DecoratedIcon, DiffStat, Disclosure, Divider, DividerColor, IconDecoration, IconDecorationKind,
-    KeyBinding, PopoverMenu, PopoverMenuHandle, SpinnerLabel, TintColor, Tooltip, WithScrollbar,
+    KeyBinding, PopoverMenu, PopoverMenuHandle, TintColor, Tooltip, WithScrollbar,
     prelude::*, right_click_menu,
 };
 use util::{ResultExt, size::format_file_size, time::duration_alt_display};
